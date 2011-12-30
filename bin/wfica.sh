d74b90db17d3b1710420767543c0c3c463c06529
Björn Andersson <ba@sanitarium.se>
2011-12-30 10:26:00 +0100

    new configuration with openbox.

---
+++ bin/wfica.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ICAROOT=/home/ba/ICAClient/linuxx86 
+export ICAROOT
+$ICAROOT/wfica -file $1
--- dot.xmonad/xmonad.hs
+++ dot.xmonad/xmonad.hs
@@ -7,11 +7,16 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.Script
 import XMonad.Actions.Submap
 import XMonad.Layout.NoBorders
+import XMonad.Layout.IM
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Reflect
 import Control.Arrow
 import Data.Bits
 import XMonad.Util.Run(spawnPipe, safeSpawn)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+
+import XMonad.Layout.Cross
 
 -- startup :: X ()
 -- myStartupHook = do
@@ -26,7 +31,7 @@ main = do
     { terminal = "urxvt",
       modMask = mod4Mask,
       keys = addPrefix (controlMask, xK_m) (keys defaultConfig),
-      layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig,
+      layoutHook = onWorkspace "9" gimp $ smartBorders $ avoidStruts $ layoutHook defaultConfig,
       manageHook = manageDocks XMonad.<+> manageHook defaultConfig,
       logHook = dynamicLogWithPP xmobarPP
                        { ppOutput = hPutStrLn xmproc
@@ -36,6 +41,10 @@ main = do
       --   spawn "xsetroot -solid black"
       --   spawn "/home/ba/bin/trayer-launcher"
     }
+    where
+      gimp = withIM (0.11) (Role "gimp-toolbox") $
+             reflectHoriz $
+             withIM (0.15) (Role "gimp-dock") Full
 
 addPrefix p ms conf =
   M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
