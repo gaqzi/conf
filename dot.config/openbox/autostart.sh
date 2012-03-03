7336ceaa6e1d59dcb25e6b799fd58019d3594474
Björn Andersson <ba@sanitarium.se>
2012-03-03 22:13:31 +0100

    ladida, more stuuuuff

--- dot.config/openbox/autostart.sh
+++ dot.config/openbox/autostart.sh
@@ -1,11 +1,10 @@
 xset b off # Beeping on errors be gone
 setxkbmap -option "ctrl:nocaps" # Caps = ctrl
 stalonetray &
-nm-applet &
+xscreensaver &
+#nm-applet &
 xfce4-power-manager &
 #bluetooth-applet &
-# gnome-volume-control-applet &
+gnome-volume-control-applet &
 #xflux -l 62.38 -g 17.3 -k 4100 > /dev/null &
 xsetroot -solid black &
-
-openbox
