9b870260551329d195b95fd669baffee33f9cd4d
Björn Andersson <ba@sanitarium.se>
2012-03-04 09:34:28 +0100

    xmonad again and global gitignore

--- dot.xmonad/xmonad.hs
+++ dot.xmonad/xmonad.hs
@@ -1,54 +1,22 @@
 import XMonad
-import System.Exit
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.Script
-import XMonad.Actions.Submap
-import XMonad.Layout.NoBorders
-import XMonad.Layout.IM
-import XMonad.Layout.PerWorkspace
-import XMonad.Layout.Reflect
-import Control.Arrow
-import Data.Bits
-import XMonad.Util.Run(spawnPipe, safeSpawn)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import XMonad.Config.Gnome
+import XMonad.Util.EZConfig
 
-import XMonad.Layout.Cross
+myManageHook = composeAll (
+    [ manageHook gnomeConfig
+    , className =? "Unity-2d-panel" --> doIgnore
+    , className =? "Unity-2d-launcher" --> doFloat
+    ])
 
--- startup :: X ()
--- myStartupHook = do
---   spawn "/usr/bin/xsetroot -solid black"
---   spawn "/home/ba/bin/trayer-launcher"
-
-main :: IO ()
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar /home/ba/.xmobarrc"
-  -- spawn "/home/ba/bin/trayer-launcher"
-  xmonad $ defaultConfig
-    { terminal = "urxvt",
-      modMask = mod4Mask,
-      keys = addPrefix (controlMask, xK_m) (keys defaultConfig),
-      layoutHook = onWorkspace "9" gimp $ smartBorders $ avoidStruts $ layoutHook defaultConfig,
-      manageHook = manageDocks XMonad.<+> manageHook defaultConfig,
-      logHook = dynamicLogWithPP xmobarPP
-                       { ppOutput = hPutStrLn xmproc
-                       , ppTitle = xmobarColor "green" "" . shorten 50
-                       },
-      startupHook = execScriptHook "startup"
-      --   spawn "xsetroot -solid black"
-      --   spawn "/home/ba/bin/trayer-launcher"
+  xmonad $ gnomeConfig {
+    terminal = "urxvt",
+    modMask = mod4Mask,
+    manageHook = myManageHook
     }
-    where
-      gimp = withIM (0.11) (Role "gimp-toolbox") $
-             reflectHoriz $
-             withIM (0.15) (Role "gimp-dock") Full
-
-addPrefix p ms conf =
-  M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
-  where
-    mod = modMask conf
-    chopMod = (.&. complement mod)
-
+    `additionalKeysP`
+    [ ("M-p", spawn "dmenu_run -b")
+    ]
+                     -- `additionalKeys`
+                     -- [ ((mod4Mask, xK_p), spawn "dmenu_run -b") -- dmenu
+                     -- ]
